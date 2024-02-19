b1a90b01f54907de7be349527df1d170bb9c0b47
HannerB <118286518+HannerB@users.noreply.github.com>
2024-02-19 17:05:48 -0500

    Add files via upload

---
+++ PWDC/mi_primera_web/#12 + 1 - Portafolio/script4.js
@@ -0,0 +1,70 @@
+let dataTable;
+let dataTableIsInitialized = false;
+
+const dataTableOptions = {
+    columnDefs: [
+        { className: "centered", targets: [0, 1, 2, 3, 4, 5] },
+        { orderable: false, targets: [4, 5] },
+        { width: "25%", targets: [5] }
+    ],
+    pageLength: 3,
+    destroy: true,
+    language: {
+        lengthMenu: "Mostrar _MENU_ registros por página",
+        zeroRecords: "Ningún usuario encontrado",
+        info: "Mostrando de _START_ a _END_ de un total de _TOTAL_ registros",
+        infoEmpty: "Ningún usuario encontrado",
+        infoFiltered: "(filtrados desde _MAX_ registros totales)",
+        search: "Buscar",
+        loadingRecords: "Cargando..",
+        paginate: {
+            first: "Primero",
+            last: "Último",
+            next: "Siguiente",
+            previous: "Anterior"
+        }
+    }
+};
+
+
+const initDataTable = async () => {
+    if (dataTableIsInitialized) {
+        dataTable.destroy();
+    }
+
+    await listUsers();
+
+    dataTable = $("#datatable_users").DataTable(dataTableOptions);
+
+    dataTableIsInitialized = true;
+};
+
+const listUsers = async () => {
+    try {
+        const response = await fetch("https://jsonplaceholder.typicode.com/users");
+        const users = await response.json();
+
+        let content = ``;
+        users.forEach((user, index) => {
+            content += `
+                <tr>
+                    <td>${index + 1}</td>
+                    <td>${user.name}</td>
+                    <td>${user.email}</td>
+                    <td>${user.address.city}</td>
+                    <td>${user.company.name}</td>
+                    <td>
+                    <button type="button" class="btn btn-warning">Editar <i class="fa-solid fa-pen-to-square"></i></button>
+                    <button type="button" class="btn btn-danger">Eliminar <i class="fa-solid fa-trash-can"></i></button>
+                    </td>
+                </tr>`;
+        });
+        tableBody_users.innerHTML = content;
+    } catch (ex) {
+        alert(ex);
+    }
+};
+
+window.addEventListener("load", async () => {
+    await initDataTable();
+});
